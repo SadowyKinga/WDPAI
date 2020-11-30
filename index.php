93952669c3dc2a32f94d3dddeb080eda7c0a2aac
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2020-12-01 00:05:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "hello world!";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/'); /*POZBYWAM SIE SLESZA I OTWARZAM SCIEZKE ODWOLUJAC SIE DO FUNKCJI SERWERA */
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController'); /*jeżeli nie mamy podanej zadnej ścieżki to odtwarzamy metode defaultcontroller*/
+Routing::get('profile', 'DefaultController'); /*scieżka do naszej apliakcji dzieki niej otwieram ścieżke project z klasy defaultcontroller*/
+Routing::run($path); /*$path - argument*/
