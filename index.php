fdf433ebaddc4af79dd736f434c24ef5c6113f34
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2020-12-12 22:49:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,5 @@ $path = parse_url($path, PHP_URL_PATH);
 
 Routing::get('login', 'DefaultController'); /*jeżeli nie mamy podanej zadnej ścieżki to odtwarzamy metode defaultcontroller*/
 Routing::get('profile', 'DefaultController'); /*scieżka do naszej apliakcji dzieki niej otwieram ścieżke project z klasy defaultcontroller*/
+Routing::get('strona', 'DefaultController'); /*scieżka do naszej apliakcji dzieki niej otwieram ścieżke project z klasy defaultcontroller*/
 Routing::run($path); /*$path - argument*/
