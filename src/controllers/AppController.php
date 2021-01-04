71e6107766793e59d7410596b867d1875413715e
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2021-01-04 12:04:53 +0100

    Update AppController.php

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,15 +1,34 @@
 <?php
+declare(strict_types=1);
 
-class AppController {
+class AppController 
+{
 
+    private $request;
+
+    public function __construct()
+    {
+        session_start();
+        $this->request = $_SERVER['REQUEST_METHOD'];
+    }
+
+    protected function isGet(): bool
+    {
+        return $this->request === 'GET';
+    }
+
+    protected function isPost(): bool
+    {
+        return $this->request === 'POST';
+    }
 /*wywołanie metody w controlerze - renderowanie szablonu htmlu*/
     protected function render(string $template = null, array $variables = []) //protected - funkcja dzielona z clasami dziedziczącymi
     {
-        $templatePath = 'public/views/'. $template.'.html'; //do nazwy szablonu doklejamy naszą ścieżkę, konkatynację stringu robimy za pomoca kropeczki(.) 
+        $templatePath = $template ? dirname(__DIR__).'//Views//'.get_class($this).'//'. $template.'.php' : ''; //do nazwy szablonu doklejamy naszą ścieżkę, konkatynację stringu robimy za pomoca kropeczki(.) 
         $output = 'File not found';
                 
         if(file_exists($templatePath)){
-          //  extract($variables); 
+            extract($variables); 
             
             ob_start(); //zapisujemy plik html do bufora
             include $templatePath; //include - wczytuje ścieżkę do pliku html
