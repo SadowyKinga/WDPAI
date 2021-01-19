1bf3e895ea81e2c91dfd7a1c423a02ed5b11d457
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2021-01-19 11:41:44 +0100

    Update Routing.php

--- src/Routing.php
+++ src/Routing.php
@@ -1,10 +1,11 @@
-
 <?php
-
 require_once 'Controllers/SecurityController.php';
 require_once 'Controllers/PageController.php';
+require_once 'Controllers/UserController.php';
 
-class Routing {
+
+class Routing
+{
     private $routes = [];
 
     public function __construct()
@@ -22,6 +23,94 @@ class Routing {
             'logout' => [
                 'controller' => 'SecurityController',
                 'action' => 'logout'
+            ],
+            'account' => [
+                'controller' => 'SecurityController',
+                'action' => 'account'
+            ],
+            'statements' => [
+                'controller' => 'UserController',
+                'action' => 'statements'
+            ],
+            'invoices' => [
+                'controller' => 'UserController',
+                'action' => 'invoices'
+            ],
+            'indexinvoices' => [
+                'controller' => 'UserController',
+                'action' => 'indexInvoices'
+            ],
+            'olderinvoices' => [
+                'controller' => 'UserController',
+                'action' => 'olderinvoices'
+            ],
+            'analysisinvoices' => [
+                'controller' => 'UserController',
+                'action' => 'analysisInvoices'
+            ],
+            'inv' => [
+                'controller' => 'UserController',
+                'action' => 'indexi'
+            ],
+            'messages' => [
+                'controller' => 'UserController',
+                'action' => 'messages'
+            ],
+            'startorders' => [
+                'controller' => 'UserController',
+                'action' => 'indexOrders'
+            ],
+            'orders' => [
+                'controller' => 'UserController',
+                'action' => 'orders'
+            ],
+            'indexorders' => [
+                'controller' => 'UserController',
+                'action' => 'indexOlderOrders'
+            ],
+            'olderorders' => [
+                'controller' => 'UserController',
+                'action' => 'olderorders'
+            ],
+            'profile' => [
+                'controller' => 'UserController',
+                'action' => 'profile'
+            ],
+            'change_pic' => [
+                'controller' => 'UserController',
+                'action' => 'change_pic'
+            ],
+            'change_data' => [
+                'controller' => 'UserController',
+                'action' => 'change_data'
+            ],
+            'change_pass' => [
+                'controller' => 'UserController',
+                'action' => 'change_pass'
+            ],
+            'graphic' => [
+                'controller' => 'UserController',
+                'action' => 'graphic'
+            ],
+            'startworkers' => [
+                'controller' => 'UserController',
+                'action' => 'indexWorkers'
+            ],
+            'workers' => [
+                'controller' => 'UserController',
+                'action' => 'workers'
+            ],
+            'delete_worker' => [
+                'controller' => 'UserController',
+                'action' => 'workerDelete'
+            ],
+            'add_worker' => [
+                'controller' => 'UserController',
+                'action' => 'workerAdd'
+            ],
+            'orderAdd' => [
+                'controller' => 'UserController',
+                'action' => 'orderAdd'
             ]
         ];
     }
@@ -37,4 +126,5 @@ class Routing {
             $object->$action();
         }
     }
+    
 }
