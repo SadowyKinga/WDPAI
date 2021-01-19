ffda473dfe0efde9fdef554457126fcc7e190973
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2021-01-19 11:35:32 +0100

    Update login.php

--- src/Views/SecurityController/login.php
+++ src/Views/SecurityController/login.php
@@ -8,16 +8,16 @@
 <body>
 <div class='container'>
     <div class='logo'>
-        <k>
-            <img src='../../public/img/Logo.png'>
-        </k>
         <s>
-            <img src='../../public/img/BrickShop.png'>
+            <img src='../../Public/img/Logo.png'>
         </s>
+        <k>
+            <img src='../../Public/img/BrickShop.png'>
+        </k>
     </div>
     <form action="?page=login" method="POST">
         <p>
-            <img src='../../public/img/Witamy.png'>
+            <img src='../../Public/img/Witamy.png'>
         </p>
         <div class="messages">
             <?php
@@ -31,11 +31,13 @@
         <div class="input_icon">
             <input name="email" type="text" placeholder="EMAIL">
             <i class="fas fa-user" aria-hidden="true"></i>
+        </div>
+        <div class="input_icon">
             <input name="password" type="password" placeholder="HASŁO">
             <i class="fas fa-lock" aria-hidden="true"></i>
         </div>
         <button class='button1' type="submit"><i class="fas fa-arrow-right fa-lg" style="color: #0B7EE0 "></i></button>
-        <button class='button2' type="button"><i class="fas fa-pen"></i><a href="?page=register">ZAREJESTRUJ SIĘ</a></button>
+        <button class='button2' type="button"><i class="fas fa-pen"></i><a href="?page=account">ZAREJESTRUJ SIĘ</a></button>
         <button class='button3' type="button"><i class="fab fa-google-plus-g"></i>ZALOGUJ SIĘ PRZEZ GOOGLE</button>
         <button class='button4' type="button"><i class="fab fa-apple"></i>ZALOGUJ SIĘ UŻYWAJĄC KONTA APPLE</button>
     </form>
